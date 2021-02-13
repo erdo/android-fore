68968eee6ffe83936ebc2b9522399c771b871f6f
erdo <226319+erdo@users.noreply.github.com>
2021-02-13 09:37:39 +0100

    bump to v1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ ext.apply {
 
     // TODO remove these once we migrate publish.gradle to kts
 
-    set("LIB_VERSION_NAME", "1.3.4")
-    set("LIB_VERSION_CODE", 48)
+    set("LIB_VERSION_NAME", "1.3.5")
+    set("LIB_VERSION_CODE", 49)
 
     set("REPO", "fore")
     set("LIB_GROUP", "co.early.fore")
