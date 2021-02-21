4ed92aeb2eb7849d1392c2ac042a28ea4b612304
erdo <226319+erdo@users.noreply.github.com>
2021-02-21 17:23:58 +0100

    moving tests to fore-jv / fore-kt

--- fore-lifecycle/build.gradle.kts
+++ fore-lifecycle/build.gradle.kts
@@ -19,15 +19,6 @@ dependencies {
     compileOnly("androidx.constraintlayout:constraintlayout:${Shared.Versions.constraintlayout}")
     compileOnly("androidx.coordinatorlayout:coordinatorlayout:${Shared.Versions.coordinatorlayout}")
     compileOnly("androidx.cardview:cardview:${Shared.Versions.cardview}")
-
-    testImplementation("junit:junit:${Shared.Versions.junit}")
-    testImplementation("org.mockito:mockito-core:${Shared.Versions.mockito_core}")
-    testImplementation("org.hamcrest:hamcrest-library:${Shared.Versions.hamcrest_library}")
-    testImplementation("com.google.code.gson:gson:${Shared.Versions.gson}")
-
-    androidTestImplementation("androidx.test.espresso:espresso-core:${Shared.Versions.espresso_core}") {
-        exclude(group = "com.android.support", module = "support-annotations")
-    }
 }
 
 apply(from = "../publish.gradle")
--- fore-network/build.gradle.kts
+++ fore-network/build.gradle.kts
@@ -26,15 +26,6 @@ dependencies {
 
     compileOnly("com.apollographql.apollo:apollo-runtime:${Shared.Versions.apollo}")
     compileOnly("com.squareup.retrofit2:retrofit:${Shared.Versions.retrofit}")
-
-    testImplementation("junit:junit:${Shared.Versions.junit}")
-    testImplementation("org.mockito:mockito-core:${Shared.Versions.mockito_core}")
-    testImplementation("org.hamcrest:hamcrest-library:${Shared.Versions.hamcrest_library}")
-    testImplementation("com.google.code.gson:gson:${Shared.Versions.gson}")
-
-    androidTestImplementation("androidx.test.espresso:espresso-core:${Shared.Versions.espresso_core}") {
-        exclude(group = "com.android.support", module = "support-annotations")
-    }
 }
 
 apply(from = "../publish.gradle")
