53eb903be206f4c4e48dd937fdfd341a1073214d
erdo <226319+erdo@users.noreply.github.com>
2021-06-05 11:53:44 +0200

    bump to v1.4.5

--- buildSrc/src/main/java/co/early/fore/Shared.kt
+++ buildSrc/src/main/java/co/early/fore/Shared.kt
@@ -54,8 +54,8 @@ object Shared {
     }
 
     object Publish {
-        const val LIB_VERSION_NAME = "1.4.4"
-        const val LIB_VERSION_CODE = 56
+        const val LIB_VERSION_NAME = "1.4.5" //"x.x.x-SNAPSHOT"
+        const val LIB_VERSION_CODE = 57
         const val LIB_GROUP = "co.early.fore"
         const val PROJ_NAME = "fore"
         const val LIB_DEVELOPER_ID = "erdo"
@@ -68,7 +68,7 @@ object Shared {
         const val LICENCE_NAME = "The Apache Software License, Version 2.0"
         const val LICENCE_URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
 
-        const val published_fore_version_for_examples = "1.4.4"
+        const val published_fore_version_for_examples = "1.4.5"
         const val use_published_version = false
     }
 
