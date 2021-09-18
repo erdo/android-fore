76666330414d243852bd8d2888a29f821111f76c
erdo <226319+erdo@users.noreply.github.com>
2021-09-18 10:30:12 +0200

    prep for Compose, add constructors to ForeLifecycleObserver

--- fore-kt-android-core/src/main/java/co/early/fore/kt/core/ui/ForeLifecycleObserver.kt
+++ fore-kt-android-core/src/main/java/co/early/fore/kt/core/ui/ForeLifecycleObserver.kt
@@ -7,13 +7,25 @@ import co.early.fore.core.observer.Observer
 import co.early.fore.core.ui.SyncableView
 import co.early.fore.kt.core.observer.ObservableGroupImp
 
-class ForeLifecycleObserver(
-    private val syncableView: SyncableView,
-    vararg observablesList: Observable
+class ForeLifeCycleObserver(
+    private val observer: Observer,
+    private val observableGroup: ObservableGroup
 ) : DefaultLifecycleObserver {
 
-    private val observableGroup: ObservableGroup = ObservableGroupImp(*observablesList)
-    private val observer: Observer = Observer { syncableView.syncView() }
+    constructor(
+        syncableView: SyncableView,
+        vararg observablesList: Observable
+    ) : this(Observer { syncableView.syncView() }, ObservableGroupImp(*observablesList))
+
+    constructor(
+        syncableView: SyncableView,
+        observableGroup: ObservableGroup
+    ) : this(Observer { syncableView.syncView() }, observableGroup)
+
+    constructor(
+        observer: Observer,
+        vararg observablesList: Observable
+    ) : this(observer, ObservableGroupImp(*observablesList))
 
     override fun onStart(owner: LifecycleOwner) {
         observableGroup.addObserver(observer)
