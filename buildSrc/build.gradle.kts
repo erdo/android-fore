6ee414b0fda53c702c4b77675eddaa94c6c1d16a
erdo <226319+erdo@users.noreply.github.com>
2022-01-29 22:25:51 +0100

    increase kotlin and compose version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,9 +14,9 @@ dependencies {
 
   // we want the kotlin and android gradle plugin, because we want to access them in our plugin
   //implementation("com.android.tools.build:gradle:${co.early.fore.Shared.Versions.android_gradle_plugin}")
-  implementation("com.android.tools.build:gradle:7.0.2")
+  implementation("com.android.tools.build:gradle:7.0.4")
   //implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${co.early.fore.Shared.Versions.kotlin_version}")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 
   //for custom plugins
   implementation(gradleApi())
