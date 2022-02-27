2a550d6d7f9999c2bca1493dea42cedb3fc2bdb1
erdo <226319+erdo@users.noreply.github.com>
2022-02-27 19:26:19 +0100

    bump compose version

--- buildSrc/src/main/java/co/early/fore/Shared.kt
+++ buildSrc/src/main/java/co/early/fore/Shared.kt
@@ -25,7 +25,7 @@ object Shared {
         const val apollo = "2.5.4"
         const val apollo3 = "3.0.0"
         const val retrofit = "2.9.0"
-        const val compose = "1.1.0-rc03"
+        const val compose = "1.1.0"
         // example app and test dependencies
         const val android_core = "1.1.0"
         const val annotation = "1.0.0"
