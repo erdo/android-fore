a7f8ac94bdfd9dac9a7290a15eb15b50bdded590
erdo <226319+erdo@users.noreply.github.com>
2025-06-24 19:13:53 +0200

    better logging for iOS

--- lib/fore-core/src/appleMain/kotlin/co/early/fore/core/logging/Logger.apple.kt
+++ lib/fore-core/src/appleMain/kotlin/co/early/fore/core/logging/Logger.apple.kt
@@ -2,37 +2,44 @@ package co.early.fore.core.logging
 
 import platform.Foundation.NSLog
 
+private const val maxChunkSize = 800
+
 internal actual fun verbose(tag: String, message: String, throwable: Throwable?) {
-    NSLog("(V) $tag|$message")
-    throwable?.let {
-        NSLog(it.toString())
-    }
+    log("(V) $tag|", message, throwable)
 }
 
 internal actual fun debug(tag: String, message: String, throwable: Throwable?) {
-    NSLog("(D) $tag|$message")
-    throwable?.let {
-        NSLog(it.toString())
-    }
+    log("(D) $tag|", message, throwable)
 }
 
 internal actual fun info(tag: String, message: String, throwable: Throwable?) {
-    NSLog("(I) $tag|$message")
-    throwable?.let {
-        NSLog(it.toString())
-    }
+    log("(I) $tag|", message, throwable)
 }
 
 internal actual fun warning(tag: String, message: String, throwable: Throwable?) {
-    NSLog("(W) $tag|$message")
+    log("(W) $tag|", message, throwable)
+}
+
+internal actual fun error(tag: String, message: String, throwable: Throwable?) {
+    log("(E) $tag|", message, throwable)
+}
+
+private fun log(preLog: String, message: String, throwable: Throwable?){
+    if (message.length > maxChunkSize){
+        NSLog(preLog)
+        longLog(message)
+    } else NSLog("$preLog$message")
     throwable?.let {
         NSLog(it.toString())
     }
 }
 
-internal actual fun error(tag: String, message: String, throwable: Throwable?) {
-    NSLog("(E) $tag|$message")
-    throwable?.let {
-        NSLog(it.toString())
+private fun longLog(message: String, chunkSize: Int = maxChunkSize) {
+    var start = 0
+    while (start < message.length) {
+        val end = (start + chunkSize).coerceAtMost(message.length)
+        val chunk = message.substring(start, end)
+        NSLog(chunk)
+        start = end
     }
 }
