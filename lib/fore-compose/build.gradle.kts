48bec901106e14386d3dcfc70984157e0abfff18
erdo <226319+erdo@users.noreply.github.com>
2026-04-25 22:43:45 +0200

    pin the published artifacts to kotlin 2.0

--- lib/fore-compose/build.gradle.kts
+++ lib/fore-compose/build.gradle.kts
@@ -2,6 +2,7 @@ import co.early.fore.Shared
 import co.early.fore.applyPublishingConfig
 import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     alias(libs.plugins.kotlinMultiPlatformPlugin)
@@ -22,6 +23,11 @@ kotlin {
 
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.jvm.toolchain.get().toInt()))
+    }
+
+    compilerOptions {
+        languageVersion.set(KotlinVersion.fromVersion(libs.versions.kotlin.floor.get()))
+        apiVersion.set(KotlinVersion.fromVersion(libs.versions.kotlin.floor.get()))
     }
 
     targets.withType<org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget> {
--- lib/fore-test-fixtures/build.gradle.kts
+++ lib/fore-test-fixtures/build.gradle.kts
@@ -4,6 +4,7 @@ import co.early.fore.Shared.Android.minSdk
 import co.early.fore.applyPublishingConfig
 import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
     alias(libs.plugins.kotlinMultiPlatformPlugin)
@@ -17,6 +18,11 @@ kotlin {
 
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.jvm.toolchain.get().toInt()))
+    }
+
+    compilerOptions {
+        languageVersion.set(KotlinVersion.fromVersion(libs.versions.kotlin.floor.get()))
+        apiVersion.set(KotlinVersion.fromVersion(libs.versions.kotlin.floor.get()))
     }
 
     targets.withType<org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget> {
